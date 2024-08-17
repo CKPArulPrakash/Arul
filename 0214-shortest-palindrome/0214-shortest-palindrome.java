d7002985ce450632ce28559a95bd716945dd14d4
CKPArulPrakash <167205688+CKPArulPrakash@users.noreply.github.com>
2024-08-17 13:56:07 +0530

    Time: 7 ms (74.46%), Space: 44.8 MB (52.45%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.java
+++ 0214-shortest-palindrome/0214-shortest-palindrome.java
@@ -1,37 +1,18 @@
 class Solution {
     public String shortestPalindrome(String s) {
-        if (s == null || s.length() == 0) {
-            return s;
-        }
-
-        String rev_s = new StringBuilder(s).reverse().toString();
-        String combined = s + "#" + rev_s;
-        int[] lps = computeLPSArray(combined);
-
-        int addLength = s.length() - lps[lps.length - 1];
-        return rev_s.substring(0, addLength) + s;
-    }
-
-    private int[] computeLPSArray(String s) {
-        int[] lps = new int[s.length()];
-        int length = 0;
-        int i = 1;
-
-        while (i < s.length()) {
-            if (s.charAt(i) == s.charAt(length)) {
-                length++;
-                lps[i] = length;
-                i++;
-            } else {
-                if (length != 0) {
-                    length = lps[length - 1];
-                } else {
-                    lps[i] = 0;
-                    i++;
-                }
+        int j=0;
+        for(int i=s.length()-1;i>=0;i--)
+        {
+            if(s.charAt(i)==s.charAt(j))
+            {
+                j++;
             }
         }
-
-        return lps;
+        if(j==s.length())
+        {
+            return s;
+        }
+       String a = s.substring(j);
+        return new StringBuffer(a).reverse().toString()+ shortestPalindrome(s.substring(0,j))+a;
     }
 }
