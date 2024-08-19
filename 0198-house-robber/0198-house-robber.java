455112cb2a6fc82b029bc5d8070a65054a511c39
CKPArulPrakash <167205688+CKPArulPrakash@users.noreply.github.com>
2024-08-19 20:19:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -5,7 +5,7 @@ class Solution
         int n=nums.length;
         int a=nums[0];
         int b=0;
-        for(int i=1;i<n;i++)
+        for(int i=1;i<=n;i++)
         {
          int pick=nums[i];
          if(i>1)
